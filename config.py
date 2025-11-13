8c9575f1f13cf62da611bbf5f0fdd79f0ed131a3
binnyw1976 <aesapozhnikov02@gmail.com>
2025-11-13 15:32:44 +0400

    1

--- config.py
+++ config.py
@@ -9,8 +9,4 @@ BASE_URL = "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 
-EMAIL_INPUT_SELECTOR = '//div[@data-testid="login-form-email-input"]//div//input'
-PASSWORD_INPUT_SELECTOR = '//div[@data-testid="login-form-password-input"]//div//input'
-LOGIN_BUTTON_SELECTOR = '//button[@data-testid="login-page-login-button"]'
-ALERT_SELECTOR = '//div[@data-testid="login-page-wrong-email-or-password-alert"]'
 ALERT_TEXT = "Wrong email or password"
--- playwright_authorization.py
+++ playwright_authorization.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
-from config import BASE_URL, EMAIL, PASSWORD, EMAIL_INPUT_SELECTOR, PASSWORD_INPUT_SELECTOR, LOGIN_BUTTON_SELECTOR, ALERT_SELECTOR, ALERT_TEXT
+from config import BASE_URL, ALERT_TEXT, EMAIL, PASSWORD
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
@@ -7,19 +7,19 @@ with sync_playwright() as playwright:
     page.goto(BASE_URL)
 
     # Заполняем email
-    email_input = page.locator(EMAIL_INPUT_SELECTOR)
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill(EMAIL)
 
     # Заполняем пароль
-    password_input = page.locator(PASSWORD_INPUT_SELECTOR)
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill(PASSWORD)
 
     # Нажимаем кнопку входа
-    login_button = page.locator(LOGIN_BUTTON_SELECTOR)
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
 
     # Проверяем появление алерта
-    wrong_email_or_password_alert = page.locator(ALERT_SELECTOR)
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text(ALERT_TEXT)
 
